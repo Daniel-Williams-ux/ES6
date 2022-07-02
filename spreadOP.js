0102f2aac14a562fc8f2a55b6d5717903a50c2a9
Daniel Williams <williamsoligodaniel@gmail.com>
2022-07-02 06:38:59 +0100

    Update spreadOP.js

--- spreadOP.js
+++ spreadOP.js
@@ -21,8 +21,44 @@ const spreaded = ...arr;
 const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
 let arr2;
 
-arr2 = arr2 = [...arr1];  // Change this line
+arr2 = [...arr1];  // Change this line
 console.log(arr2);
 
 /*Code Explanation
 Unpacking the arr1 using the spread operator and then copying those values to arr2*/
+ 
+
+
+//IMPORTANT USES OF THE SPREAD OPERATOR
+
+//1.Using spread operator to concatenate
+arr1 = ["Thywill"];
+arr2 = ["Olayemi"];
+arr1 = [...arr1,...arr2];
+console.log(arr1) //["Thywill", "Olayemi"]
+
+//2.copy arrays
+arr1 = [1, 2, 3,];
+arr2 = [...arr1];
+arr2.push(4)
+console.log(arr1)//[1, 2, 3,]
+console.log(arr2)//[1, 2, 3, 4]
+
+//3.Add the element of an existing array into a new array
+const day1 = ['fri', 'sat',];
+const day2 = ['mon', 'tues', 'wed', 'thurs', ...day1, 'sun'];
+console.log(day2);//['mon', 'tues', 'wed', 'thurs', 'fri', 'sat', 'sun'];
+
+//pass elements of an array as arguments to a function
+let nums = [1, 2, 3];
+function addThreeNumbers(x, y, z) {
+  return 1 + 2 + 3;
+}
+console.log(addThreeNumbers(...nums));//6
+
+let nums = [1, 2, 3];
+function addThreeNumbers(x, y, z) {
+  let nums = [1, 2, 3];
+  console.log(x+y+z)
+}
+addThreeNumbers(...nums);//6
